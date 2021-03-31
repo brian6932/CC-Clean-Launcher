1a4136982c6dac48c0480c15d8f6eb6d789fbf55
Brian <18603393+brian6932@users.noreply.github.com>
2021-03-31 09:08:31 -0400

    Add files via upload

---
+++ CC_Exit.bat
@@ -0,0 +1,8 @@
+taskkill /f /im "Adobe Desktop Service.exe"
+taskkill /f /im "AdobeIPCBroker.exe"
+taskkill /f /im "CCXLibrary.exe"
+taskkill /f /im "CCXProcess.exe"
+taskkill /f /im "CoreSync.exe"
+taskkill /f /im "Creative Cloud Helper.exe"
+taskkill /f /im "node.exe"
+taskkill /f /im "CCLibrary.exe"
