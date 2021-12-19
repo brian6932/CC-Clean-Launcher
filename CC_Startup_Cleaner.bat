30bfb730676baefca4e344eb620eb015f753c8b1
Brian <18603393+brian6932@users.noreply.github.com>
2021-12-19 16:06:51 -0500

    forgot to comment out node in the other script

--- CC_Startup_Cleaner.bat
+++ CC_Startup_Cleaner.bat
@@ -4,7 +4,7 @@ timeout 4
 taskkill /f /im "Adobe Desktop Service.exe"
 taskkill /f /im "CoreSync.exe"
 taskkill /f /im "Creative Cloud Helper.exe"
-taskkill /f /im "node.exe"
+: taskkill /f /im "node.exe"
 timeout 20
 taskkill /f /im "AdobeIPCBroker.exe"
 taskkill /f /im "CCXLibrary.exe"
