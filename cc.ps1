f490fdea05244141bcc71aba49cded0d99c11c1f
Brian <18603393+brian6932@users.noreply.github.com>
2023-12-26 15:09:08 -0500

    Add Illustrator

--- cc.ps1
+++ cc.ps1
@@ -6,21 +6,23 @@ $program = $host.ui.PromptForChoice(
         [ChoiceDescription]::new('Photo&shop'),
         [ChoiceDescription]::new('&Premiere Pro'),
         [ChoiceDescription]::new('&Media Encoder'),
-        [ChoiceDescription]::new('&After Effects')
+        [ChoiceDescription]::new('&After Effects'),
+        [ChoiceDescription]::new('&Illustrator')
     ),
     $null
 )
 $program = switch ($program) {
-    0 { 'Adobe Photoshop *\Photoshop.exe' }
-    1 { 'Adobe Premiere Pro *\Adobe Premiere Pro.exe' }
-    2 { 'Adobe Media Encoder *\Adobe Media Encoder.exe' }
-    3 { 'Adobe After Effects *\Support Files\AfterFX.exe' }
+    0 { 'Adobe Photoshop */Photoshop.exe' }
+    1 { 'Adobe Premiere Pro */Adobe Premiere Pro.exe' }
+    2 { 'Adobe Media Encoder */Adobe Media Encoder.exe' }
+    3 { 'Adobe After Effects */Support Files/AfterFX.exe' }
+    4 { 'Adobe Illustrator */Support Files/Contents/Windows/Illustrator.exe' }
 }
 $job = Start-Job -ScriptBlock {
     $program = $using:program
     $proc_name = $program.Substring(($idx = $program.IndexOf('\') + 1), $program.Length - $idx - 4)
     $killable = [System.Collections.Generic.HashSet[string]]('Adobe Crash Processor', 'AdobeIPCBroker', 'CCXProcess', 'CCLibrary', 'node', 'msedgewebview2')
-    $proc = Start-Process -PassThru "C:\Program Files\Adobe\$program"
+    $proc = Start-Process -PassThru "C:/Program Files/Adobe/$program"
     Start-Sleep 20
     foreach ($_ in (Get-Process | Sort-Object -Descending -Property StartTime)) {
         if ($_.Name -eq $proc_name) {
